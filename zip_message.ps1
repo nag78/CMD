63e76cb3b4481deee71a920cc3a226efc3f9f063
Nikolay Goltsev <nag78@yandex.ru>
2019-02-07 15:15:52 +0300

    v 0.2

--- zip_message.ps1
+++ zip_message.ps1
@@ -2,10 +2,7 @@ param([string]$arg1)
 $Dir = $arg1
 $INN = '7712038455'
 $DateS = '20190131'
-$DD = "{0:dd}" -f [datetime]::Now
-$MM = "{0:MM}" -f [datetime]::Now
-$YYYY = "{0:yyyy}" -f [datetime]::Now
-$DateF = $YYYY+$MM+$DD
+$DateF = Get-date -f {yyyyMMdd}
 $file = $INN + '_' + $DateS + '_' + $DateF + '_' + $arg1 + '.zip'
 
 Compress-Archive -Path .\$Dir\*.* -DestinationPath $Dir\$file -CompressionLevel NoCompression
