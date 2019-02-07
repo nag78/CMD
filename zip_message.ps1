28da5c1145543616934a12407c16f9626822e32f
Nikolay Goltsev <nag78@yandex.ru>
2019-02-07 15:38:40 +0300

    v 0.3

--- zip_message.ps1
+++ zip_message.ps1
@@ -1,8 +1,16 @@
+# Получение аргументом названия каталога и порядкового номера
 param([string]$arg1)
 $Dir = $arg1
+
+# Задание параметров ИНН ДатаСоставления
 $INN = '7712038455'
 $DateS = '20190131'
+
+# Получение сегодняшней ДатаФормирования
 $DateF = Get-date -f {yyyyMMdd}
+
+# Формирование имени zip - файла ИНН_ДатаСоставления_ДатаФормирования_порядковый номер.zip
 $file = $INN + '_' + $DateS + '_' + $DateF + '_' + $arg1 + '.zip'
 
+# Добавление файлов в архив без компрессии
 Compress-Archive -Path .\$Dir\*.* -DestinationPath $Dir\$file -CompressionLevel NoCompression
