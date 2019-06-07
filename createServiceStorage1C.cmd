115cac58f5bea9b8077be568e89e1e442ed4708f
Nikolay Goltsev <nag78@yandex.ru>
2019-06-07 17:35:48 +0300

    Update createServiceStorage1C.cmd

--- createServiceStorage1C.cmd
+++ createServiceStorage1C.cmd
@@ -1,5 +1,5 @@
-REM ╨б╨╛╨╖╨┤╨░╨╜╨╕╨╡ ╨╜╨╡╤Б╨║╨╛╨╗╤М╨║╨╕╤Е ╤Б╨╗╤Г╨╢╨▒, ╤А╨░╨╖╨╜╤Л╤Е ╨▓╨╡╤А╤Б╨╕╨╣ ╤Е╤А╨░╨╜╨╕╨╗╨╕╤Й╨░ ╨║╨╛╨╜╤Д╨╕╨│╤Г╤А╨░╤Ж╨╕╨╣ 1╨б.
+REM Создание нескольких служб, разных версий хранилища конфигураций 1С.
 
-sc create "1C_Storage_8.3.9.2233"  binPath= "C:\Program Files (x86)\1cv8\8.3.9.2233\bin\crserver.exe" -srvc -port 1542 -range 1560:1591 -d D:\storage\" start= auto displayname= "╨б╨╡╤А╨▓╨╡╤А ╤Е╤А╨░╨╜╨╕╨╗╨╕╤Й╨░ 1╨б (╨С╨У╨г_╨Ч╨Ъ╨С╨г_╨б╨в╨Ш╨Я)"
-REM sc create "1C_Storage_8.3.11.2867"  binPath= "C:\Program Files (x86)\1cv8\8.3.11.2867\bin\crserver.exe" -srvc -port 1642 -range 1660:1691 -d D:\storage\" start= auto displayname= "╨б╨╡╤А╨▓╨╡╤А ╤Е╤А╨░╨╜╨╕╨╗╨╕╤Й╨░ 1╨б (8.3.11.2867)"
-sc create "1C_Storage_8.3.8.1933"  binPath= "C:\Program Files (x86)\1cv8\8.3.8.1933\bin\crserver.exe" -srvc -port 1542 -range 1560:1591 -d D:\storage\" start= auto displayname= "╨б╨╡╤А╨▓╨╡╤А ╤Е╤А╨░╨╜╨╕╨╗╨╕╤Й╨░ 1╨б (╨Р╨б╨г╨Я╨Ъ_╨Ш╨Р╨б╨г)"
+sc create "1C_Storage_8.3.9.2233"  binPath= "C:\Program Files (x86)\1cv8\8.3.9.2233\bin\crserver.exe" -srvc -port 1542 -range 1560:1591 -d D:\storage\" start= auto displayname= "Сервер хранилища 1С (БГУ_ЗКБУ_СТИП)"
+REM sc create "1C_Storage_8.3.11.2867"  binPath= "C:\Program Files (x86)\1cv8\8.3.11.2867\bin\crserver.exe" -srvc -port 1642 -range 1660:1691 -d D:\storage\" start= auto displayname= "Сервер хранилища 1С (8.3.11.2867)"
+sc create "1C_Storage_8.3.8.1933"  binPath= "C:\Program Files (x86)\1cv8\8.3.8.1933\bin\crserver.exe" -srvc -port 1542 -range 1560:1591 -d D:\storage\" start= auto displayname= "Сервер хранилища 1С (АСУПК_ИАСУ)"
