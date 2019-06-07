4bb6cb9314c7e4c78cd3e5ddd1956d03a618a0eb
Nikolay Goltsev <nag78@yandex.ru>
2019-06-07 19:01:57 +0300

    Update createServiceStorage1C.cmd

--- createServiceStorage1C.cmd
+++ createServiceStorage1C.cmd
@@ -1,5 +1,5 @@
 REM �������� ��᪮�쪨� �㦡, ࠧ��� ���ᨩ �࠭���� ���䨣��権 1�.
 
-sc create "1C_Storage_8.3.9.2233"  binPath=""""C:\Program Files (x86)\1cv8\8.3.9.2233\bin\crserver.exe""" -srvc -port 1542 -range 1560:1591 -d """D:\storage\"""" start=auto displayname= "��ࢥ� �࠭���� 1� (���_����_����)"
+sc create "1C_Storage_8.3.9.2233"  binPath= ""C:\Program Files (x86)\1cv8\8.3.9.2233\bin\crserver.exe" -srvc -port 1542 -range 1560:1591 -d D:\storage\"" start=auto displayname= "��ࢥ� �࠭���� 1� (���_����_����)"
 REM sc create "1C_Storage_8.3.11.2867"  binPath= "C:\Program Files (x86)\1cv8\8.3.11.2867\bin\crserver.exe" -srvc -port 1642 -range 1660:1691 -d D:\storage\" start= auto displayname= "��ࢥ� �࠭���� 1� (8.3.11.2867)"
-sc create "1C_Storage_8.3.8.1933"  binPath=""""C:\Program Files (x86)\1cv8\8.3.8.1933\bin\crserver.exe""" -srvc -port 1742 -range 1760:1791 -d """D:\storage\"""" start=auto displayname= "��ࢥ� �࠭���� 1� (�����_����)"
+sc create "1C_Storage_8.3.8.1933"  binPath= ""C:\Program Files (x86)\1cv8\8.3.8.1933\bin\crserver.exe" -srvc -port 1742 -range 1760:1791 -d "D:\storage\"" start=auto displayname= "��ࢥ� �࠭���� 1� (�����_����)"
