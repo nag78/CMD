77fb7a113e6c5c8b5ae23ff085c13585229601d8
Nikolay Goltsev <nag78@yandex.ru>
2019-06-10 18:02:49 +0300

    Up

---
+++ create_stor1C8.3.11.2867.ps1
@@ -0,0 +1,4 @@
+﻿$Path = 'C:\Program Files (x86)\1cv8\8.3.11.2867\bin\crserver.exe'
+$pathFull = $Path + ' -srvc -port 1742 -range 1760:1791 -d D:\storage'
+
+New-Service -Name 1C_Storage_8.3.11.2867 -BinaryPathName $pathFull -StartupType Automatic -DisplayName "1C Хранилище (8.3.11.2867)"
--- create_stor1C8.3.8.1933.ps1
+++ create_stor1C8.3.8.1933.ps1
@@ -1,4 +1,4 @@
 ﻿$Path = 'C:\Program Files (x86)\1cv8\8.3.8.1933\bin\crserver.exe'
-$pathFull = $Path_1 + ' -srvc -port 1642 -range 1660:1691 -d D:\storage'
+$pathFull = $Path + ' -srvc -port 1642 -range 1660:1691 -d D:\storage'
 
-New-Service -Name 1C_Storage_8.3.8.1933 -BinaryPathName $pathFull_2 -StartupType Automatic -DisplayName "1C Хранилище (8.3.8.1933)"
+New-Service -Name 1C_Storage_8.3.8.1933 -BinaryPathName $pathFull -StartupType Automatic -DisplayName "1C Хранилище (8.3.8.1933)"
