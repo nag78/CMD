aa8faa76728b4737e5dd69ebf83b17fa7473b94d
Nikolay Goltsev <nag78@yandex.ru>
2019-06-10 13:30:12 +0300

    Create create_stor1C8.3.9.2233.ps1

---
+++ create_stor1C8.3.9.2233.ps1
@@ -0,0 +1,4 @@
+﻿$Path = 'C:\Program Files (x86)\1cv8\8.3.9.2233\bin\crserver.exe'
+$pathFull = $Path + ' -srvc -port 1542 -range 1560:1591 -d D:\storage' 
+
+New-Service -Name 1C_Storage_8.3.9.2233 -BinaryPathName $pathFull_1 -StartupType Automatic -DisplayName "1C Хранилище (8.3.9.2233)"
