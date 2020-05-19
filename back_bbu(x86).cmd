df9e33fffcde2297bcde180842fdffceea62009c
nag78 <nag78@yandex.ru>
2020-05-19 17:18:16 +0300

    Update back_bbu(x86).cmd

--- back_bbu(x86).cmd
+++ back_bbu(x86).cmd
@@ -3,14 +3,14 @@ if not exist R:\ net use R: \\172.16.10.116\1c-backup\ /YES
 
 set YY=%date:~6,4%
 set DT=%date%
-set filename = R:\ï¿½ï¿½ï¿½\%YY%\%date%_bgu.dt
+set filename = R:\“\%YY%\%date%_bgu.dt
 set logfile = R:\LOG\bgu_backup.log
 set exe = "C:\Program Files\1cv8\8.3.9.2233\bin\1cv8.exe"
 set srv = "1cserever\bgu"
 set user = "Nag"
 set pass = "3edCVfr4"
 
-IF NOT EXIST R:\ï¿½ï¿½ï¿½\%YY% MKDIR R:\ï¿½ï¿½ï¿½\%YY%
+IF NOT EXIST R:\“\%YY% MKDIR R:\“\%YY%
 if exist %filename% EXIT
 
 echo %DT% >> %logfile%
