0ffe55a8843a7d1a70965017d1effe9d3f85acaa
Nikolay Goltsev <nag78@yandex.ru>
2021-06-30 16:54:40 +0300

    Update AppServers.ps1

--- AppServers.ps1
+++ AppServers.ps1
@@ -1,5 +1,7 @@
 $servers = @('SRV1','SRV2','SRV3','SRV4', 'SRV5')
 if (Test-Connection -ComputerName $servers[0] -Quiet -Count 1){
     Get-Content -Path "\\$($servers[0])\c$\App_configurqtion.txt"
+} else {
+    Write-Error -Message "The server $($servers[0]) is not responding!"
 }
 Get-Content -Path "\\$($servers[1])\c$\App_configuration.txt"
