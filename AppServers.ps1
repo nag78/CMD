94eb4496b82f9965c80bed3194bce9abcad379dc
Nikolay Goltsev <nag78@yandex.ru>
2021-07-15 15:18:27 +0300

    Update AppServers.ps1

--- AppServers.ps1
+++ AppServers.ps1
@@ -10,6 +10,12 @@ if (-not (Test-Connection -ComputerName $servers[0] -Quiet -Count 1)) {
     Write-Error -Message "The server $servers[0] is not responding!"
 } elseif ($server[0] -eq $problemServer)
     Write-Error -Message "The server $servers[0] does not have the right file!"
-} else {
+else {
     Get-Content -Path "\\$servers[0]\c$\App_configuration.txt"
+}
+
+$currentServer = $servers[0]
+switch ($currentServer) {
+    condition { $servers[0] }
+    Default {}
 }
