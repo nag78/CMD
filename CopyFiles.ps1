e3f3c90012cd5ae8e4f9aded54ff88e9771f9bfc
Nikolay Goltsev <nag78@yandex.ru>
2021-07-21 13:32:31 +0300

    Comments

--- CopyFiles.ps1
+++ CopyFiles.ps1
@@ -6,10 +6,12 @@ $target = 'J:\'
 
 New-SmbMapping -UserName $username -Password $password `
     -LocalPath 'J:' -RemotePath '\\172.16.0.216\change' 
-
+#Получение имени последнего, самого свежего файла и добавление его в путь копирования
 $file = Get-ChildItem -Path $path | Sort-Object LastWriteTime | Select-Object -Last 1
 $fullpath = $path + $file.Name
+#Удаление преведущего файла в месте назначения
 Remove-Item $target 
+#Копирование файла и отключение сетевого диска
 if (Copy-Item -Path $fullpath -Destination $target -Verbose) {
     Net Use J: /delete
 }
