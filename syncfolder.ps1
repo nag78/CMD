2d2a39b8806103406e6bbed5c01f7e99d67e6519
Nikolay Goltsev <nag78@yandex.ru>
2021-07-26 14:14:43 +0300

    Create syncfolder.ps1

---
+++ syncfolder.ps1
@@ -0,0 +1,14 @@
+$Folder1Path = 'H:\actual_backup'
+$Folder2Path = 'H:\change_backup'
+$folder1Files = Get-ChildItem -Recurse -path $Folder1Path
+$folder2Files = Get-ChildItem -Recurse -path $Folder2Path
+$file_Diffs = Compare-Object -ReferenceObject $folder1Files -DifferenceObject $folder2Files
+$file_Diffs | 
+  foreach {
+     $copyParams = @{'Path' = $_.InputObject.FullName}
+     if($_.SideIndicator -eq '<=' )
+     { 
+         $copyParams.Destination = $_.InputObject.FullName -replace [regex]::Escape($Folder1Path),$Folder2Path
+     }
+     copy-Item @copyParams -force
+}
