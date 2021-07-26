c7caf08666dc7e9436a49269f3ffe294c9524a82
Nikolay Goltsev <nag78@yandex.ru>
2021-07-26 14:16:02 +0300

    Update syncfolder.ps1

--- syncfolder.ps1
+++ syncfolder.ps1
@@ -4,7 +4,7 @@ $folder1Files = Get-ChildItem -Recurse -path $Folder1Path
 $folder2Files = Get-ChildItem -Recurse -path $Folder2Path
 $file_Diffs = Compare-Object -ReferenceObject $folder1Files -DifferenceObject $folder2Files
 $file_Diffs | 
-  foreach {
+  ForEach-Object {
      $copyParams = @{'Path' = $_.InputObject.FullName}
      if($_.SideIndicator -eq '<=' )
      { 
